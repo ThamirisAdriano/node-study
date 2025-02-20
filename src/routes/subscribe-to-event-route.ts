91e3cc30d37d499655a7c518993ede62ec21899a
thamiris <tsadriano.band.com.br>
2025-02-20 10:51:26 -0300

    second commit

--- src/routes/subscribe-to-event-route.ts
+++ src/routes/subscribe-to-event-route.ts
@@ -1,5 +1,6 @@
 import type { FastifyPluginAsyncZod } from 'fastify-type-provider-zod'
-import z from 'zod'
+import { z } from 'zod'
+import { subscribeToEvent } from '../functions/subscribe-to-event'
 
 export const subscribeToEventRoute: FastifyPluginAsyncZod = async app => {
   app.post(
@@ -7,15 +8,14 @@ export const subscribeToEventRoute: FastifyPluginAsyncZod = async app => {
     {
       schema: {
         summary: 'Subscribes someone to the event',
-        tags: ['subscriptions'],
+        tags: ['subscription'],
         body: z.object({
           name: z.string(),
           email: z.string().email(),
         }),
         response: {
           201: z.object({
-            name: z.string(),
-            email: z.string(),
+            subscriberId: z.string(),
           }),
         },
       },
@@ -23,9 +23,13 @@ export const subscribeToEventRoute: FastifyPluginAsyncZod = async app => {
     async (request, reply) => {
       const { name, email } = request.body
 
-      return reply.status(201).send({
+      const { subscriberId } = await subscribeToEvent({
         name,
         email,
+      })
+
+      return reply.status(201).send({
+        subscriberId,
       })
     }
   )
